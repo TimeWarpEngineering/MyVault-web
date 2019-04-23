542efdc83c9d01e26a5d5d202b7292721a8f01b5
stackbaxter <stackbaxter@yahoo.com>
2019-04-23 15:48:49 -0500

    fixed names

---
+++ Source/TypeScript/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path"); const webpack = require("webpack"); module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: [ ".ts", ".js" ]
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  entry: {
+    "clientjs": "./source/Initialize.ts"
+  },
+  output: {
+    path: path.join(__dirname,
+    "/dist"),
+    filename: "[name].js"
+  }
+};
