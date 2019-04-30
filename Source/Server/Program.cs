61835b16a106e087b4b82ee274c539d44f17ad0a
stackbaxter <stackbaxter@yahoo.com>
2019-04-30 18:49:52 -0500

    style was weird, just in case I returned wwwroot to server, until it's known for sure it is just a .scss compile issue.

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -1,45 +1,17 @@
 ﻿namespace Server
 {
-  using System;
-  using Server.Data;
-  using Microsoft.AspNetCore;
   using Microsoft.AspNetCore.Hosting;
-  using Microsoft.Extensions.Configuration;
-  using Microsoft.Extensions.DependencyInjection;
-  using Microsoft.Extensions.Logging;
   using Microsoft.Extensions.Hosting;
 
   public class Program
   {
-    public static IWebHost BuildWebHost(string[] aArgumentArray) =>
-          WebHost
-            .CreateDefaultBuilder(aArgumentArray)
-            .UseConfiguration(new ConfigurationBuilder()
-              .AddCommandLine(aArgumentArray)
-              .Build())
-            .UseStartup<Startup>()
-            .Build();
+    public static IHostBuilder CreateHostBuilder(string[] aArgumentArray) =>
+      Host.CreateDefaultBuilder(aArgumentArray)
+        .ConfigureWebHostDefaults(aWebHostBuilder =>
+          {
+            aWebHostBuilder.UseStartup<Startup>();
+          });
 
-
-    public static void Main(string[] aArgumentArray)
-    {
-      IWebHost host = BuildWebHost(aArgumentArray);
-
-      //using (IServiceScope scope = host.Services.CreateScope())
-      //{
-      //  System.IServiceProvider services = scope.ServiceProvider;
-      //  try
-      //  {
-      //    AnthemGoldPwaDbContext anthemGoldPwaDbContext = services.GetRequiredService<AnthemGoldPwaDbContext>();
-      //    DbInitializer.Initialize(anthemGoldPwaDbContext);
-      //  }
-      //  catch (Exception exception)
-      //  {
-      //    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
-      //    logger.LogError(exception, "An error occurred creating the DB.");
-      //  }
-      //}
-      host.Run();
-    }
+    public static void Main(string[] aArgumentArray) => CreateHostBuilder(aArgumentArray).Build().Run();
   }
 }
