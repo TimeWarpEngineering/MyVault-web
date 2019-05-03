6c787b2e8d3d420a5e54edda4e625b585ed9ff14
stackbaxter <stackbaxter@yahoo.com>
2019-05-02 23:02:57 -0500

    vain attempts at the ConversionResponse.

--- Source/Client/Features/Edge/Components/Wallet/Wallet.razor.cs
+++ Source/Client/Features/Edge/Components/Wallet/Wallet.razor.cs
@@ -1,18 +1,20 @@
 ﻿namespace Client.Features.Edge.Components.Wallet
 {
   using System;
+  using System.Net.Http;
   using System.Threading.Tasks;
   using Client.Components;
   using Client.Features.Edge.EdgeCurrencyWallet;
   using Microsoft.AspNetCore.Components;
   using Shared.Features.Conversion;
-  //using System.Net.Http;
+
 
   public class WalletModel : BaseComponent
     {
 
         [Parameter] protected EdgeCurrencyWallet EdgeCurrencyWallet { get; set; }
 
+        private HttpClient HttpClient { get; }
         public string Balance => EdgeCurrencyWallet.SelectedCurrencyCode != null ? EdgeCurrencyWallet?.Balances[EdgeCurrencyWallet.SelectedCurrencyCode] : null;
 
         public string CurrencyCode => EdgeCurrencyWallet.SelectedCurrencyCode ?? "AGLD";
@@ -21,22 +23,23 @@
 
         public void OnClickHandler(string aCurrencyCode) => EdgeCurrencyWallet.SelectedCurrencyCode = aCurrencyCode;
 
-    public ConversionResponse ConversionResponse { get; set; }
+        public ConversionResponse ConversionResponse { get; set; }
 
-    //public HttpClient HttpClient { get; set; }
+        public ConversionRequest aConversionRequest { get; set; }
 
-        public ConversionRequest ConversionRequest { get; set; } 
-                
 
-        public void PrintStuff()
-        {   ConversionRequest = new ConversionRequest("Hello", "again");
+        public async Task PrintStuff()
+        {
+        ConversionResponse = new ConversionResponse();
+            aConversionRequest = new ConversionRequest("agld", "usd");
+          Console.WriteLine(aConversionRequest.ToCurrency + "theConversionrequest");
+           Console.WriteLine(ConversionRequest.Route + "conversion route");
 
-      Console.WriteLine(CurrencyCode);
 
-            Console.WriteLine(ConversionRequest.FromCurrency);
-      Console.WriteLine(ConversionRequest.ToCurrency);
-      Console.WriteLine(ConversionRequest.Route);
-            Console.WriteLine("Stuff!!!!");
+        ConversionResponse = await HttpClient.SendJsonAsync<ConversionResponse>(HttpMethod.Get, HttpClient, ConversionRequest.Route, aConversionRequest);
+            Console.WriteLine(ConversionResponse.Rate + "response");
+            
+
         }
 
         //private Task<T> HttpClient<T>(string route, ConversionRequest conversionRequest) => throw new NotImplementedException();
@@ -44,8 +47,8 @@
         //Console.WriteLine(response + "response here maybe");
         //          return response;
         //        }
-}
   }
+}
 
 
   //FromCurrency=AGLD&ToCurrency=USD"
