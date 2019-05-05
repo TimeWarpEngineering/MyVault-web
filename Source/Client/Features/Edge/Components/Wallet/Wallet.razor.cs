35e3f8b48779eee8f3008997c497d31d076c1e2f
Steven T. Cramer <SteveTCramer@gmail.com>
2019-05-05 02:43:26 +0000

    Display Conversion Rate
    Don't need special constructor for Conversion Request can use initializtion
    Update Async call to BFF to get ConversionRate

--- Source/Client/Features/Edge/Components/Wallet/Wallet.razor.cs
+++ Source/Client/Features/Edge/Components/Wallet/Wallet.razor.cs
@@ -7,52 +7,34 @@
   using Client.Features.Edge.EdgeCurrencyWallet;
   using Microsoft.AspNetCore.Components;
   using Shared.Features.Conversion;
-
+  using TimeWarp.Extensions;
 
   public class WalletModel : BaseComponent
+  {
+    public string Balance => EdgeCurrencyWallet.SelectedCurrencyCode != null ? EdgeCurrencyWallet?.Balances[EdgeCurrencyWallet.SelectedCurrencyCode] : null;
+    public ConversionResponse ConversionResponse { get; set; }
+    public string CurrencyCode => EdgeCurrencyWallet.SelectedCurrencyCode ?? "AGLD";
+    public int Granularity => EdgeCurrencyWallet.Granularity[CurrencyCode];
+    [Parameter] protected EdgeCurrencyWallet EdgeCurrencyWallet { get; set; }
+
+    [Inject] protected HttpClient HttpClient { get; set; }
+
+    public void OnClickHandler(string aCurrencyCode) => EdgeCurrencyWallet.SelectedCurrencyCode = aCurrencyCode;
+
+    protected override async Task OnInitAsync()
     {
+      Console.WriteLine("WalletModel OnInitAsync");
+      var conversionRequest = new ConversionRequest
+      {
+        FromCurrency = "agld",
+        ToCurrency = "usd"
+      };
 
-        [Parameter] protected EdgeCurrencyWallet EdgeCurrencyWallet { get; set; }
+      // The HttpClient.Base Address is configured before registering with the DI container.
+      string uri = $"{ConversionRequest.Route}?{conversionRequest.ToQueryString()}";
+      ConversionResponse = await HttpClient.GetJsonAsync<ConversionResponse>(uri);
 
-        private HttpClient HttpClient { get; }
-        public string Balance => EdgeCurrencyWallet.SelectedCurrencyCode != null ? EdgeCurrencyWallet?.Balances[EdgeCurrencyWallet.SelectedCurrencyCode] : null;
-
-        public string CurrencyCode => EdgeCurrencyWallet.SelectedCurrencyCode ?? "AGLD";
-
-        public int Granularity => EdgeCurrencyWallet.Granularity[CurrencyCode];
-
-        public void OnClickHandler(string aCurrencyCode) => EdgeCurrencyWallet.SelectedCurrencyCode = aCurrencyCode;
-
-        public ConversionResponse ConversionResponse { get; set; }
-
-        public ConversionRequest aConversionRequest { get; set; }
-
-
-        public async Task PrintStuff()
-        {
-        ConversionResponse = new ConversionResponse();
-            aConversionRequest = new ConversionRequest("agld", "usd");
-          Console.WriteLine(aConversionRequest.ToCurrency + "theConversionrequest");
-           Console.WriteLine(ConversionRequest.Route + "conversion route");
-
-
-        ConversionResponse = await HttpClient.SendJsonAsync<ConversionResponse>(HttpMethod.Get, HttpClient, ConversionRequest.Route, aConversionRequest);
-            Console.WriteLine(ConversionResponse.Rate + "response");
-            
-
-        }
-
-        //private Task<T> HttpClient<T>(string route, ConversionRequest conversionRequest) => throw new NotImplementedException();
-        //var response = await HttpClient.SendJsonAsync<ConversionResponse>(HttpMethod.Get, "api/Conversion", ConversionRequest);
-        //Console.WriteLine(response + "response here maybe");
-        //          return response;
-        //        }
+      StateHasChanged();
+    }
   }
 }
-
-
-  //FromCurrency=AGLD&ToCurrency=USD"
-
-  
-  
-
