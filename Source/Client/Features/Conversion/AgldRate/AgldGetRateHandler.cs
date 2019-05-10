7b1d2e9a489e6e0dc9355c00aab5dc9dd5cdbba2
stackbaxter <stackbaxter@yahoo.com>
2019-05-10 17:37:16 -0500

    I think I'm close

--- Source/Client/Features/Conversion/AgldRate/AgldGetRateHandler.cs
+++ Source/Client/Features/Conversion/AgldRate/AgldGetRateHandler.cs
@@ -4,21 +4,28 @@ using System.Threading.Tasks;
 using Shared.Features.Conversion;
 using System.Net.Http;
 using Microsoft.AspNetCore.Components;
-
+// I would like to have one Conversion State that has the values for both AGLD and Eth and the EdgeWallet.CurrencyCode 
+// will pick which one is used applicable.
 namespace Client.Features.Conversion.AgldRate
 {
   public partial class AgldRateState
   {
     public class AgldGetRateHandler : RequestHandler<AgldGetRateAction, AgldRateState>
     {
-      public AgldGetRateHandler(IStore aStore) : base(aStore)
+      public AgldGetRateHandler(IStore aStore, HttpClient aHttpClient) : base(aStore)
       {
+        HttpClient = aHttpClient;
       }
+      private HttpClient HttpClient { get; }
       //AgldRateState AgldRateState => Store.GetState<AgldRateState>();
+      ConversionRequest ConversionRequest = new ConversionRequest() { FromCurrency = "agld", ToCurrency = "usd" };
 
-      public override async Task<AgldGetRateAction> Handle(ConversionRequest aConversionRequest, CancellationToken ACancellationToken)
+      public override async Task<AgldRateState> Handle(AgldGetRateAction aAgldGetRateAction, CancellationToken ACancellationToken)
       {
-       ConversionResponse ConversionResponse  = await Send<ConversionResponse>(ConversionRequest.Route);
+
+        ConversionResponse ConversionResponse = await HttpClient.GetJsonAsync<ConversionResponse>(ConversionRequest.Route);
+
+
       }
 
 
