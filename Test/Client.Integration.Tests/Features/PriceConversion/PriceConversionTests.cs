9b5e4cddc92ad094e830548b6b4cd67d5f67816b
stackbaxter <stackbaxter@yahoo.com>
2019-05-13 09:06:00 +0000

    because tests

---
+++ Test/Client.Integration.Tests/Features/PriceConversion/PriceConversionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Client.Integration.Tests.Features.PriceConversion
+{
+  using FluentValidation.Results;
+  using Server.Services.AnthemGold.Price;
+  using System;
+  using static Server.Services.AnthemGold.AnthemGoldConstants;
+  public class IsValidPriceRequest
+  {
+    // Arrange
+
+    readonly PriceRequest priceRequest = new PriceRequest();
+
+    PriceRequestValidator priceRequestValdiator = new PriceRequestValidator();
+    
+    // Assert
+    PriceRequestValidator priceRequestValidator { get; set; }
+
+    public ValidationResult Result => result;
+
+    readonly ValidationResult result = priceRequestValidator.Validate(priceRequest)
+
+  }
+
+
+
+}
