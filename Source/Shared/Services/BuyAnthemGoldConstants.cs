5a9abb99624c764e2dd115b931fbedd1d77e8ddf
Steven T. Cramer <SteveTCramer@gmail.com>
2019-05-13 02:43:31 +0000

    Logan desired links to Buy anthem gold site added to menu.
    Add some constants to avoid magic strings

---
+++ Source/Shared/Services/BuyAnthemGoldConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Services
+{
+  public class BuyAnthemGoldConstants
+  {
+    public const string MyAccountUrl = "https://buy.anthemgold.com/dashboard";
+    public const string MyLedgerUrl = "https://buy.anthemgold.com/dashboard/purchases/list";
+    public const string BunkerAssetsUrl = "http://assets.anthemgold.com/bd-1.0-SNAPSHOT/asset-list.jsp";
+    public const string BuyGoldUrl = "https://buy.anthemgold.com/purchase/crypto";
+  }
+}
