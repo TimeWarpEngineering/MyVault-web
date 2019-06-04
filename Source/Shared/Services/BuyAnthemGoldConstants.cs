7d1b7ae34fa60f603cf456c284827bf5912b60f2
stackbaxter <stackbaxter@yahoo.com>
2019-06-04 10:45:36 -0500

    So the asset site loads

--- Source/Shared/Services/BuyAnthemGoldConstants.cs
+++ Source/Shared/Services/BuyAnthemGoldConstants.cs
@@ -4,7 +4,7 @@
   {
     public const string MyAccountUrl = "https://buy.anthemgold.com/dashboard";
     public const string MyLedgerUrl = "https://buy.anthemgold.com/dashboard/purchases/list";
-    public const string BunkerAssetsUrl = "http://assets.anthemgold.com/bd-1.0-SNAPSHOT/asset-list.jsp";
+    public const string BunkerAssetsUrl = "https://assets.anthemgold.com/bd-1.0-SNAPSHOT/asset-list.jsp";
     public const string BuyGoldUrl = "https://buy.anthemgold.com/purchase/crypto";
   }
 }
